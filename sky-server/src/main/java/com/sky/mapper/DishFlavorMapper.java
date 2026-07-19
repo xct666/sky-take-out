daa5a64385989812ad177601081085a4406f124f
jacktao <2584599642@qq.com>
2026-07-19 20:46:06 +0800

    商品浏览业务功能

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -22,4 +22,7 @@ public interface DishFlavorMapper {
      */
     @Delete("delete  from dish_flavor where dish_id = #{dishId}")
     void deleteByDishId(Long dishId);
+
+
+    List<DishFlavor> getByDishId(Long id);
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -4,6 +4,7 @@ import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
 
 import java.util.List;
 
@@ -40,4 +41,11 @@ public interface DishService {
      * @return
      */
     List<Dish> list(Long categoryId);
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
